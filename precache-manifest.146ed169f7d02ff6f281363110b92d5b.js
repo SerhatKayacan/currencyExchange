6a682fa52e588628baac9abc8079359ac26e28fc
Serhat Kayacan <serhatkayacan006@gmail.com>
2019-02-27 20:31:24 +0300

    Updates

---
+++ precache-manifest.146ed169f7d02ff6f281363110b92d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a8c0cd8485d7077427fdd1614ab691",
+    "url": "/currencyExchange/static/media/christine-roy-343235-unsplash.31a8c0cd.jpg"
+  },
+  {
+    "revision": "4a4d453757cd0e746e39",
+    "url": "/currencyExchange/static/js/runtime~main.4a4d4537.js"
+  },
+  {
+    "revision": "368c40533bd1597ebb0b",
+    "url": "/currencyExchange/static/js/main.368c4053.chunk.js"
+  },
+  {
+    "revision": "9cfd9d0f0da26469a5d6",
+    "url": "/currencyExchange/static/js/2.9cfd9d0f.chunk.js"
+  },
+  {
+    "revision": "368c40533bd1597ebb0b",
+    "url": "/currencyExchange/static/css/main.81d66268.chunk.css"
+  },
+  {
+    "revision": "9cfd9d0f0da26469a5d6",
+    "url": "/currencyExchange/static/css/2.d500cc58.chunk.css"
+  },
+  {
+    "revision": "3577d443cc8b2e29b86d38d66edc0539",
+    "url": "/currencyExchange/index.html"
+  }
+];
